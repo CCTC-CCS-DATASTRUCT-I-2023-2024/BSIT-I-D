7e1bcad4d965391f97a2ebfd7d48568b6abd52bb
JOHNLOYD-svg <156911022+JOHNLOYD-svg@users.noreply.github.com>
2024-02-23 08:52:13 +0800

    Add files via upload

---
+++ Activities/Activity I/GUTIB_ACTIVITY_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x = 20;
+    int y = 30;
+    int z = 40;
+
+    int Product = x * y * z;
+    int Sum = x + y + z;
+    int Difference = x - y - z;
+    int Qoutient = x / y / z;
+
+    cout << "Product is: " << Product << endl;
+    cout << "Sum is: " << Sum << endl;
+    cout << "Difference is: " << Difference << endl;
+    cout << "Qoutient is: " << Qoutient << endl;
+}
