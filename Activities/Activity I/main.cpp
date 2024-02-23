b946da1c5e617968f9914f44e80e635601bdcf31
James Ivan Jimenez <156911118+jamesezrampage12@users.noreply.github.com>
2024-02-23 09:16:26 +0800

    Add files via upload

---
+++ Activities/Activity I/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+        int a = 10;
+        int b = 15;
+        int c = 20;
+
+        int sum = a+b+c;
+        int diff = c-b-a;
+        int product = a*b*c;
+        int quo =c/b/a;
+
+        cout <<"sum is: "<<sum <<endl;
+        cout <<"diff is: "<<diff << endl;
+        cout <<"product is: "<< product<<endl;
+        cout <<"quotient is: " << quo <<endl;
+
+        }
