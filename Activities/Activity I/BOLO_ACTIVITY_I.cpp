16d0d0863f928907571f8442aece96b29b1818d9
jhonzkie201 <157588320+jhonzkie201@users.noreply.github.com>
2024-02-23 09:19:47 +0800

    Add files via upload

---
+++ Activities/Activity I/BOLO_ACTIVITY_I.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     
+     int x = 15;
+     int y = 30;
+     int z = 45;
+
+     int sum = x + y + z;
+     int difference = x - y - z;
+     int product = x * y * z;
+     int qoutient = x / y / z;
+
+     cout <<"The sum is:" << sum << endl;
+     cout <<"The diff is:" << difference << endl;
+     cout <<"The prod is:" << product << endl;
+     cout <<"The qout is:" << qoutient << endl;
+
+
+
+    
+}
