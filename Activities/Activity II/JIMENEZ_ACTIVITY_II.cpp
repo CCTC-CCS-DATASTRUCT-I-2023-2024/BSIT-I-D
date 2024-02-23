7eb7e4fc3670a30f561bf2c44b38b43e323ce661
James Ivan Jimenez <156911118+jamesezrampage12@users.noreply.github.com>
2024-02-23 09:26:18 +0800

    Add files via upload

---
+++ Activities/Activity II/JIMENEZ_ACTIVITY_II.cpp
@@ -0,0 +1,25 @@
+##include <iostream>
+using namespace std;
+
+int main(){
+        int num1;
+        int num2;
+
+        cout << "Enter Values: ";
+        cin >> num1;
+
+        cout <<"Enter Values: ";
+        cin >> num2;
+
+        int sum = num1 + num2;
+        int prod = num1 * num2;
+        int qou = num1 - num2;
+        int diff = num1 / num2;
+
+        cout <<"Sum is: " << sum << endl;
+        cout <<"Product is: " << prod << endl;
+        cout <<"Qoutient is: " << qou << endl;
+        cout <<"Difference is: " << diff << endl;
+
+
+}
