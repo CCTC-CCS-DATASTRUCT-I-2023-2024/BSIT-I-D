60de6fa889d5a13ced413ae608e32b5fed93affc
Kathleen Merano <157017491+mkathleenn@users.noreply.github.com>
2024-02-23 09:34:19 +0800

    Add files via upload

---
+++ Activities/Activity I/MERANO_ACTIVITY_I.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+   
+   int x=2, y=8, z=10;
+
+   int sum = x+y+z;
+   int diff = x-y-z;
+   int product = x*y*z;
+   int quotient = x/y/x;
+   
+   cout << sum << endl;
+   cout << diff << endl;
+   cout << product << endl;
+   cout << quotient << endl;
+
+
+}
