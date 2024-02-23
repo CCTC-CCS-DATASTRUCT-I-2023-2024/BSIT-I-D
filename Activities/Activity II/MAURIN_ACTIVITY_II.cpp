0f5a68de574da6627a246624e949cf7677330667
shekinamariemaurin <156909161+shekinamariemaurin@users.noreply.github.com>
2024-02-23 09:36:21 +0800

    Add files via upload

---
+++ Activities/Activity II/MAURIN_ACTIVITY_II.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+
+    int num1, num2;
+        
+    cout << "Enter value ";
+    cin >> num1 ;
+
+    cout<< "Enter value ";
+    cin >> num2 ;
+
+    int sum = num1 + num2;
+    int difference = num2 - num1;
+    int product = num1*num2;
+        
+    cout << "Sum is " << sum << endl;
+    cout << "Difference is " << difference << endl;
+    cout << "Product is " << product << endl;
+
+}
+
