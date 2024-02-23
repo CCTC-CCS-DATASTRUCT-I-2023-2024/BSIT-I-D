130baa0aa30177c6c66b9956406e590480bc7e45
Balongcas <157291749+AljhoBalongcas@users.noreply.github.com>
2024-02-23 10:01:51 +0800

    Add files via upload

---
+++ Activities/Activity VI/BALONGCAS_ACTIVITY_VI.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int count;
+    cout<<"Input your number: ";
+    cin>>count;
+
+    while(count >=0){
+        cout<< count <<endl;
+        count--;
+    }
+}
