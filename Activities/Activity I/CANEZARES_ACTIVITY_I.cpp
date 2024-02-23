d1c781508d4855f64a04331bf9565186fe275b12
Carlo P. Cañezares <156906555+CarloPC@users.noreply.github.com>
2024-02-23 08:42:07 +0800

    Add files via upload

---
+++ Activities/Activity I/CANEZARES_ACTIVITY_I.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+        int x = 10;
+        int y = 20;
+        int z = 30;
+
+        int Product = x * y * z;
+        int Sum = x + y + z;
+        int Difference = x - y - z;
+        int Qoutient = x / y / z;
+
+    cout <<"Product is: " << Product << endl;   
+    cout <<"Sum is: " << Sum << endl;
+    cout <<"Difference is: " << Difference << endl;
+    cout <<"Qoutient is: " << Qoutient << endl;
+    
+}
