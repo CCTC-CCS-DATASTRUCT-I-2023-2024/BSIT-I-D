f08551ec31c2da26c2f043646debbf23795beab1
patrick0907 <158041084+patrick0907@users.noreply.github.com>
2024-02-23 09:20:43 +0800

    Add files via upload

---
+++ Activities/Activity I/FAJARDO.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()  {
+
+    int a = 10, b = 15, c = 20;
+
+    int sum = a + b;
+    int prod = c * b;
+    int diff = b - a;
+    int quotient = c / a;
+
+    cout << "Sum " << sum; 
+    cout << "Prod " << prod;
+    cout << "Diff " << diff; 
+    cout << "Quotient " << quotient; 
+
+ 
+
+
+}
