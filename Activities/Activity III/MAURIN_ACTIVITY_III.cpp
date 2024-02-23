ee558cc06a6104abf93edb85d2fa8fa83f79de1d
shekinamariemaurin <156909161+shekinamariemaurin@users.noreply.github.com>
2024-02-23 09:37:27 +0800

    Add files via upload

---
+++ Activities/Activity III/MAURIN_ACTIVITY_III.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Enter your age: ";
+    cin >>age;
+
+    if (age >= 16){
+        cout << "Welcome" <<endl;
+    }else{
+        cout << "Not Allowed" <<endl;
+    }
+    
+
+}
