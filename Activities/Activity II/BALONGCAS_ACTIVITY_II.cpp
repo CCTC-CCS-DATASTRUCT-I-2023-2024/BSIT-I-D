1e982192ea648e9033412c6ea4578611887fd8bf
Balongcas <157291749+AljhoBalongcas@users.noreply.github.com>
2024-02-23 09:31:52 +0800

    Add files via upload

---
+++ Activities/Activity II/BALONGCAS_ACTIVITY_II.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int  main(){
+
+    int fn, sn;
+    cout <<"First Number: ";
+    cin>>fn;
+    cout <<"Second Number: ";
+    cin>>sn;
+
+
+        int Sum = fn + sn;
+        int Diff = fn - sn;
+        int Pro = fn * sn;
+        int Quo = fn / sn;
+
+        cout<<"Sum= "<< Sum<<endl;
+        cout<<"Diff= "<< Diff<<endl;
+        cout<<"Pro= "<< Pro<<endl;
+        cout<<"Quo= "<< Quo<<endl;
+
+}
