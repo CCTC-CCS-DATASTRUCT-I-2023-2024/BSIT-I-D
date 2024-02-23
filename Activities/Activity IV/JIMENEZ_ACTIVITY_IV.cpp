bfc54c13b3d9315e96cbd68ec0dcfccee4571535
James Ivan Jimenez <156911118+jamesezrampage12@users.noreply.github.com>
2024-02-23 09:45:18 +0800

    Add files via upload

---
+++ Activities/Activity IV/JIMENEZ_ACTIVITY_IV.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    /*Espresso
+    Americano
+    Cappuccino
+    Latte  */
+
+    int num;
+
+    cout << "Enter your choice of Coffee: ";
+    cin >> num;
+
+    switch(num){
+        case 1:
+            cout <<"Espresso" << endl;
+            break;
+        case 2:
+            cout <<"Americano" << endl;
+            break;
+        case 3:
+            cout <<"Cappuccino" << endl;
+            break;
+        case 4:
+            cout <<"Latte" << endl;
+            break;
+    }
+}
