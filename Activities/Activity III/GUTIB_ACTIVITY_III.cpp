e18b04c773991c0dacd1e08da53b66af44d01edc
JOHNLOYD-svg <156911022+JOHNLOYD-svg@users.noreply.github.com>
2024-02-23 09:30:12 +0800

    Add files via upload

---
+++ Activities/Activity III/GUTIB_ACTIVITY_III.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age;
+    cout << "Enter your Age: ";
+    cin >> age;
+
+    if (age >= 19)
+    {
+        cout << "Yoyoyoyoyo" << endl;
+    }
+    else
+    {
+        cout << "Not Good" << endl;
+    }
+}
