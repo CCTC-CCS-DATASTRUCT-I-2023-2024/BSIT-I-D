46e9a11f34b69ba883c025a1282611e67907900e
Balongcas <157291749+AljhoBalongcas@users.noreply.github.com>
2024-02-23 09:37:29 +0800

    Add files via upload

---
+++ Activities/Activity III/BALONGCAS_ACTIVITY_III.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+     int temp;
+     cout<<"Temperature: ";
+     cin>>temp;
+    
+    if(temp>=100){
+        cout<<"Boiling";
+    }else
+    cout<<"Not Boiling";
+}
