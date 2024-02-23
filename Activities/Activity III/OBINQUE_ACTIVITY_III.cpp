adb023cc20016709c2003bcb737c14bb15f72765
obinque <157590125+obinque@users.noreply.github.com>
2024-02-23 09:32:31 +0800

    Add files via upload

---
+++ Activities/Activity III/OBINQUE_ACTIVITY_III.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cin >> age;
+
+    if (age >= 16) {
+        cout << "Welcome" << endl;
+    } else {
+        cout << "Not allowed" << endl;
+    }
+
+   
+}
