502dde8162ae9107e2815271ec7b5b4ca4ea949c
obinque <157590125+obinque@users.noreply.github.com>
2024-02-23 09:33:04 +0800

    Add files via upload

---
+++ Activities/Activity IV/OBINQUE_ACTIVITY_IV.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int choice;
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            cout << "Espresso" << endl;
+            break;
+        case 2:
+            cout << "Americano" << endl;
+            break;
+        case 3:
+            cout << "Cappuccino" << endl;
+            break;
+        case 4:
+            cout << "Latte" << endl;
+            break;
+        default:
+            cout << "Invalid choice." << endl;
+    }
+
+   
+}
