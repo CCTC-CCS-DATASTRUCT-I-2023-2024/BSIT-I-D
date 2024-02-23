9d082f35e4e91a19f92aca4c001bae26fbb33880
crislyn18 <157588791+crislyn18@users.noreply.github.com>
2024-02-23 09:28:54 +0800

    Add files via upload

---
+++ Activities/Activity II/SURIG_ACTIVITY_II.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int s;
+    int n;
+    cout << "Enter first number: ";
+    cin >> s;
+    cout << "Enter second number: ";
+    cin >> n;
+
+    int sum = s + n;
+    int diff = s - n;
+    int prod = s * n;
+
+    cout << "Sum = " << sum << endl;
+    cout << "Difference = " << diff << endl;
+    cout << "Product = " << prod << endl;
+
+    
+}
