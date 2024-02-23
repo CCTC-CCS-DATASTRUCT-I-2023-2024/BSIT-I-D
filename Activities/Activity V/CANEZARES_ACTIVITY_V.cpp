ae337ae991e22faa8813ec49f953a5f6efd3b70e
Carlo P. Cañezares <156906555+CarloPC@users.noreply.github.com>
2024-02-23 09:28:05 +0800

    Add files via upload

---
+++ Activities/Activity V/CANEZARES_ACTIVITY_V.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+    cout <<"Enter Age: ";
+    cin >> age;
+
+    if(age >= 0 && age <=3){
+        cout <<"Free" << endl;
+    }else if(age >= 4 && age <=6){
+        cout <<"Discounted" << endl;
+    }else 
+        cout <<"Normal" << endl;
+
+}
