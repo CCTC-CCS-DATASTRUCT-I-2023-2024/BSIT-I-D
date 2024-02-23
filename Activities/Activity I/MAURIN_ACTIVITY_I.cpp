91758e7a1a38a9e3e765557947c3f6d84e07fee7
shekinamariemaurin <156909161+shekinamariemaurin@users.noreply.github.com>
2024-02-23 09:35:55 +0800

    Add files via upload

---
+++ Activities/Activity I/MAURIN_ACTIVITY_I.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a = 10;
+    int b = 15;
+    int c = 20;
+
+    int sum = a+b;
+    int difference = c-b;
+    int product = b*b;
+    int quotient = c/a;
+
+    cout <<"Sum is " << sum << endl;
+    cout <<"Difference is " << difference << endl;
+    cout <<"Product is " << product  << endl;
+    cout <<"Quotient is " << quotient << endl;
+
+}
