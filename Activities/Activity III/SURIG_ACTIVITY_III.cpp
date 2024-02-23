44d7989337d5aebe323f01e11c1dc945f3f20761
crislyn18 <157588791+crislyn18@users.noreply.github.com>
2024-02-23 09:29:41 +0800

    Add files via upload

---
+++ Activities/Activity III/SURIG_ACTIVITY_III.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+    cout <<"Enter your age: ";
+    cin >> age;
+
+    if(age >=16){
+        cout <<"Welcome";
+    }else{
+        cout <<"Not Allowed";
+    }
+    
+    return 0;
+}
