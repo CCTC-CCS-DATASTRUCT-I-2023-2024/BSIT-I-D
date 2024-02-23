ed01c1b9505df98e7e7c79e91c830d18dd81446e
carl160 <156910039+carl160@users.noreply.github.com>
2024-02-23 09:40:02 +0800

    Add files via upload

---
+++ Activities/Activity I/PAUSATOR_ACTIVITY_I.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main();
+      int x = 5;
+      int y = 15;
+      int z = 20;
+
+      int addition = x+y+z;
+      int subtraction = addition-z;
+      int multiplication = addition*subtraction;
+      int division = multiplication/addition;
+
+
+      count <<"sum="<< subtraction<<end1;
+      count <<"Diff"= "<< subtration<<end1;
+      count << "product= " << multiplication<<end1;
+      count <<"Quontient= " << division<<end1;
+
+    
+
+
