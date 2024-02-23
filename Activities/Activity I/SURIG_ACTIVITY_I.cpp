f61a0fd05b198fac7ec7daa8a78f5a698d45fd3b
crislyn18 <157588791+crislyn18@users.noreply.github.com>
2024-02-23 09:27:48 +0800

    Add files via upload

---
+++ Activities/Activity I/SURIG_ACTIVITY_I.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a = 10; 
+    int b = 50;
+    int c = 70;
+
+    int sum = a + b;
+    int sub = a - c;
+    int mul = a * b;
+    int div = a / c;
+
+    cout << "Sum = " << sum << endl;
+    cout << "Sub = " << sub << endl;
+    cout << "Mul = " << mul << endl;
+    cout << "Div = " << div << endl;
+
+
+}
