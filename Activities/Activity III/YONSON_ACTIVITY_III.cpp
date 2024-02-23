fc6fcda5346b263089978073ff16d204c6b7f2eb
Yonsonlesbert <157588466+Yonsonlesbert@users.noreply.github.com>
2024-02-23 09:52:09 +0800

    Add files via upload

---
+++ Activities/Activity III/YONSON_ACTIVITY_III.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<("Enter age: ")<<endl;
+    cin>>age;
+    if (age>=16) {
+        cout<<"Welcome"16<<endl;
+    } else{
+
+        cout<<"Not Allowed"<<endl;
+    }
+
+}
