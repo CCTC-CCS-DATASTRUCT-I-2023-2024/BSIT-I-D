4d0292f173c9cedc80c7be70c2a7c378beca55c4
obinque <157590125+obinque@users.noreply.github.com>
2024-02-23 09:59:58 +0800

    Add files via upload

---
+++ Activities/Activity I/OBINQUE_ACTIVITY_I.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    int sum = a+b+c;
+    int difference = c-b+a;
+    int product = b*a*c;
+    int qoutient = c/a/b;
+
+    cout <<"Sum is " << sum << endl;
+    cout <<"Difference " << difference << endl;
+    cout <<"Product " << product << endl;
+    cout <<"Qoutient " << qoutient << endl;
+
+    
+}
