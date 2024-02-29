052be9d93b93948b3764ad2828c572edb6908c96
Leonises <157588921+calunsagleonises@users.noreply.github.com>
2024-02-29 23:06:09 +0800

    Add files via upload

---
+++ Activities/Activity I/CALUNSAG_ACTIVITY_I.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 100, y = 200, z = 300;
+
+    int sum = x+y+z, diff = x-y-z, prod = x*y*z, quo = x/y/z;
+
+    cout << "Sum of:  "  <<sum <<endl;
+    cout << "Difference of:  "  <<diff <<endl;
+    cout << "Product of: "  <<prod <<endl;
+    cout << "Qoutient of: "  <<quo <<endl;
+
+
+
+}
+
