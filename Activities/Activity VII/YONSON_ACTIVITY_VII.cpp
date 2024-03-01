f47281a9d0d7136c6c3bff4c607be6219b478d2a
Yonsonlesbert <157588466+Yonsonlesbert@users.noreply.github.com>
2024-03-01 10:17:48 +0800

    Add files via upload

---
+++ Activities/Activity VII/YONSON_ACTIVITY_VII.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int num;
+    int sum = 0;
+
+    cout <<"Enter a non-negative integer: ";
+    cin >> num;
+
+    for(int i = 1; i <= num; i++){
+        sum +=i;
+    }
+    
+    cout << "The sum of all whole numbers from 1 to "<<fac<<" is "<<sum<<endl;
+
+}
