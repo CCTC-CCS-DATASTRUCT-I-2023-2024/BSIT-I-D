4027b4219e9aa078d580e055a475d1b98744ded0
johnkenneth1 <157591295+johnkenneth1@users.noreply.github.com>
2024-03-01 11:07:45 +0800

    Add files via upload

---
+++ Activities/Activity V/UDIAMAN_ACTIVITY_V.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age >= 0 && age <= 5)
+    {
+        cout << "IM OKAY" << endl;
+    }
+    else if (age >= 7 && age <= 12)
+    {
+        cout << "NOT OKAY" << end;
+    }
+    else 
+        cout << "ALWAYS OKAY" << end;
+
+}    
+       
