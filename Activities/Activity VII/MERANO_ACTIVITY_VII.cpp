e3e06e58a20db193c7821c691d200d3d08714580
Kathleen Merano <157017491+mkathleenn@users.noreply.github.com>
2024-03-06 09:39:06 +0800

    Add files via upload

---
+++ Activities/Activity VII/MERANO_ACTIVITY_VII.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    long num;
+    long sum;
+
+    cout << "Enter your number: ";
+    cin >> fac;
+
+    for(long i = 1; i <= num; i++){
+         sum += i;
+    }
+         
+     cout << "The sum of all whole numbers from 1 to " << num << " is " << sum << endl;
+}
