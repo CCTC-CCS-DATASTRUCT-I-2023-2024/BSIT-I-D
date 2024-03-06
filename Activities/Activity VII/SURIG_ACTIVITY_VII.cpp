ef711b4733155d76a2da44766e06c36f03d4ff76
crislyn18 <157588791+crislyn18@users.noreply.github.com>
2024-03-06 09:54:56 +0800

    Add files via upload

---
+++ Activities/Activity VII/SURIG_ACTIVITY_VII.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long num;
+    long sum;
+    cout<<("Enter Factorial number : ");
+    cin>>num;
+    for (long i = 1; i<=num; i++){
+        sum+=i;
+      }
+    cout<<"The sum of all whole numbers from 1 to "<<num<<" is "<<sum<<endl;
+
+
+}
