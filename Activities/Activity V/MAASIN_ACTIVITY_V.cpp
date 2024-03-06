16286dfe685112ca4506c1ea5bad5d62dab61ab4
jhonaroldmaasin1 <158531223+jhonaroldmaasin1@users.noreply.github.com>
2024-03-06 10:36:04 +0800

    Add files via upload

---
+++ Activities/Activity V/MAASIN_ACTIVITY_V.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age;
+    cout << "Enter your Age: ";
+    cin >> age;
+
+    if (age >= 0 && age <= 4)
+    {
+        cout << "For me" << endl;
+    }
+    else if (age >= 6 && age <= 10)
+    {
+        cout << "Iloveyou" << endl;
+    }
+    else
+        cout << "Your property" << endl;
+}
