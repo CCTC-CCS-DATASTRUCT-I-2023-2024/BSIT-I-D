81916fd79170453c163b473362237a71ec09f57b
Kathleen Merano <157017491+mkathleenn@users.noreply.github.com>
2024-03-06 10:01:49 +0800

    Update MERANO_ACTIVITY_VII.cpp

--- Activities/Activity VII/MERANO_ACTIVITY_VII.cpp
+++ Activities/Activity VII/MERANO_ACTIVITY_VII.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main(){
 
-    long num;
-    long sum;
+    int num;
 
     cout << "Enter your number: ";
     cin >> num;
 
-    for(long i = 1; i <= num; i++){
+    long sum;
+
+    for(int i = 1; i <= num; i++){
          sum += i;
-    }
-         
+    }         
      cout << "The sum of all whole numbers from 1 to " << num << " is " << sum << endl;
 }
