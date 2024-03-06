8004032ae8f9685349f0210b18a1790089d8b49a
allegofaith <156910544+allegofaith@users.noreply.github.com>
2024-03-06 10:09:29 +0800

    Add files via upload

---
+++ Activities/Activity IV/ALLEGO_ACTIVITY_IV.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int choice;
+
+cout<< "Enter your choice: ";
+cin>> choice;
+
+switch(choice){
+    case 1:
+      cout<< "Espresso";
+      break;
+    case 2:
+      cout<< "Americano";
+      break;
+    case 3:
+      cout<< "Cappuccino";
+      break;
+    case 4:
+      cout<< "Latte";
+      break;
+      
+     default: 
+     cout<< "NA";
+    
+    
+}
+     
+     
+     
+}
+    
+    
+    
+    
+
+
+
+   
