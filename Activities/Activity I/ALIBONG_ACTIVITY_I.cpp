f4b89c173ff8adf27563ce6bf2db1ec0cfc8e8ae
laarnie04 <159782611+laarnie04@users.noreply.github.com>
2024-03-06 10:38:15 +0800

    Add files via upload

---
+++ Activities/Activity I/ALIBONG_ACTIVITY_I.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+
+    int num1 = 5;
+    int num2 = 10;
+    int num3 = 15;
+
+    
+    int addition = num1 + num2 + num3;
+    int subtraction = num1 - num2 - num3;
+    int multiplication = num1 * num2 * num3;
+    float division = (float)num1 / num2;
+
+    cout << "Result of addition: " << addition << endl;
+    cout << "Result of subtraction: " << subtraction << endl;
+    cout << "Result of multiplication: " << multiplication << endl;
+    cout << "Result of division: " << division << endl;
+
+    return 0;
+}
