4d8568a94e54a82e808efcd8a94b9c31c2d5c935
patrick0907 <158041084+patrick0907@users.noreply.github.com>
2024-03-08 09:48:03 +0800

    Add files via upload

---
+++ Activities/Activity VI/FAJARDO_ACTIVITY_VI.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    string Transaction;
+    int total;
+    
+    cout <<"Do you want to start a transaction? (yes/no): ";
+    cin >> Transaction;
+
+    while(Transaction == "yes"){
+        int price;
+
+        cout <<"Enter the price of the item: ";
+        cin >> price;
+
+        total += price;
+
+        cout <<"Do you want to add another item? (yes/no: )";
+        cin >> Transaction;
+    }
+
+        cout << "Total price: " << total ;
+
+}
