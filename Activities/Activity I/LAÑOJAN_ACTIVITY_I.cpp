76f349af34745dd611a2dbd1d93425e999c596e5
Iamjoseph21 <156908222+Iamjoseph21@users.noreply.github.com>
2024-03-08 09:32:01 +0800

    LAÑOJAN_ACTIVITY_I.cpp

--- Activities/Activity I/LAÑOJAN_ACTIVITY_I.cpp
+++ Activities/Activity I/LAÑOJAN_ACTIVITY_I.cpp
@@ -8,7 +8,7 @@ int diff = a-b;
 int product = a*b*c;
 int quo = a*b;
 cout<<"sum is" << sum <<endl;
-cout<<"fiff is" << diff <<endl;
+cout<<"diff is" << diff <<endl;
 cout<<"product is" << product <<endl;
 cout<<"quotient is" << quo <<endl;
 return 0;
