69a22ea85f7be4b2ed304864a728323a32d99dcf
Lydivine Dalida <157588963+lydivinegakodlida04@users.noreply.github.com>
2024-03-08 09:28:53 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/DALIDA-ACTIVITY_I.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string names[5];
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter name " << (i + 1) << ": ";
+        cin >>(names[i]);
+    }
+
+    cout << "Enter the number of the name you would like to select (1-5): ";
+
+
+    int number;
+    cin >> number;
+    
+
+    if (number >= 1 && number<= 5) {
+        cout << "Selected name: " << names[number - 1] << endl;
+    } else {
+        cout << "Invalid selection. Please choose a number between 1 and 5." << endl;
+    }
+
+    
+}
+
