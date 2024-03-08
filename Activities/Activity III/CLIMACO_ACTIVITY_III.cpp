e5114e459586e6fcc5e777cb0b2b7b03376c8511
Anilov Hector P. Climaco <151500404+R3shaded@users.noreply.github.com>
2024-03-08 09:33:39 +0800

    Add files via upload

---
+++ Activities/Activity III/CLIMACO_ACTIVITY_III.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age;
+    cout << "Enter your Age: ";
+    cin >> age;
+
+    if (age >= 19)
+    {
+        cout << "Hello World" << endl;
+    }
+    else
+    {
+        cout << "Hello again world" << endl;
+    }
+}
