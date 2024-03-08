8c904041b3bd344b267bd152a610c2e1c6e9159f
Iamjoseph21 <156908222+Iamjoseph21@users.noreply.github.com>
2024-03-08 09:30:36 +0800

    Add files via upload

---
+++ Activities/Activity I/LAÑOJAN_ACTIVITY_I.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a =10, b= 5,  c=11 ;
+int sum = a + b + c;
+int diff = a-b;
+int product = a*b*c;
+int quo = a*b;
+cout<<"sum is" << sum <<endl;
+cout<<"sum is" << diff <<endl;
+cout<<"sum is" << product <<endl;
+cout<<"sum is" << quo <<endl;
+return 0;
+}
