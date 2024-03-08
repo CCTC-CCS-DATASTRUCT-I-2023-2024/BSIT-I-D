8c5510adb8ecc7e3bb5cfd978b0ae3d830b83720
alissonperales <157588373+alissonperales@users.noreply.github.com>
2024-03-07 17:34:08 -0800

    Add files via upload

---
+++ Activities/PERALES_ACTIVITY I.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+
+    int num1 = 20;
+    int num2 = 15;
+    int num3 = 5;
+
+    
+    int addition = num1 + num2 + num3;
+    int subtraction = num1 - num2 - num3;
+    int multiplication = num1 * num2 * num3;
+    float division = (float)num1 / num2;
+
+    std::cout << "Result of addition: " << addition << std::endl;
+    std::cout << "Result of subtraction: " << subtraction << std::endl;
+    std::cout << "Result of multiplication: " << multiplication << std::endl;
+    std::cout << "Result of division: " << division << std::endl;
+
+    return 0;
+}
