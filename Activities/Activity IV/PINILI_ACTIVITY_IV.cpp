461c0055cf3a5fcc039d04e109af89114b6d82f4
piniliace <158040288+piniliace@users.noreply.github.com>
2024-03-08 09:45:26 +0800

    Add files via upload

---
+++ Activities/Activity IV/PINILI_ACTIVITY_IV.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+    cout<< "Enter your choice: ";
+    cin >> num;
+
+    switch(num){
+        case 1:
+        cout << "Espresso" <<endl;
+        break;
+        case 2:
+        cout << "Americano" <<endl;
+        break;
+        case 3:
+        cout << "Cappuccino" <<endl;
+        break;
+        case 4:
+        cout << "Latte" <<endl;
+        break; 
+
+        default:
+        cout<<"Invalid choice" <<endl;
+        break;
+    }
+     return 0;
+}
