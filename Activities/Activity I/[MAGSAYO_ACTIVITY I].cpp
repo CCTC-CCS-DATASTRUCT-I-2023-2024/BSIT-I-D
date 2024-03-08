5d528355074d710c596eae06725645f901f69024
JayEstre <156909272+JayEstre@users.noreply.github.com>
2024-03-08 09:37:42 +0800

    Add files via upload

---
+++ Activities/Activity I/[MAGSAYO_ACTIVITY I].cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 50, b = 50, c = 43;
+    int sum = a + b + c;
+    int diff = a - b - c;
+    int prod = a * b * c;
+    int quo = a / c / b;
+    
+    cout << "The sum is " << sum << endl;
+    cout << "The difference is " << diff << endl;
+    cout << "The product is " << prod << endl;
+    cout << "The quotient is " << quo << endl;
+    
+}
