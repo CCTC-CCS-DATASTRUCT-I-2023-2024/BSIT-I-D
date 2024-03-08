08106fae41717c2c0729a6e52843b74c9efe08f9
Leonises <157588921+calunsagleonises@users.noreply.github.com>
2024-03-08 11:38:04 +0800

    Add files via upload

---
+++ Activities/Activity II/CALUNSAG_ACTIVITY_II.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1;
+    int num2;
+    cout<<("Enter Value: ")<<endl;
+    cin>>num1;
+    cout<<("Enter Value: ")<<endl;
+    cin>>num2;
+
+    int sum = num1+num2;
+    int diff = num1-num2;
+    int prod = num1*num2;
+    int Qou = num1 / num2;
+    
+    cout<<"Sum is "<<sum<<endl;
+    cout<<"Difference is "<<diff<<endl;
+    cout<<"Product is "<<prod<<endl;
+    cout<<"Qoutient is "<<Qou<<endl;
+
+}
