2dbfe815a0fbe33ef825c407be574f216f5b6ef7
LuckyCarredo <159015794+LuckyCarredo@users.noreply.github.com>
2024-03-08 09:37:02 +0800

    Add files via upload

---
+++ Activities/Activity III/CARREDO_ACTIVITY_III.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age;
+    cout << "Enter your Age: ";
+    cin >> age;
+
+    if (age >= 19)
+    {
+        cout << "Bro Bro Bro" << endl;
+    }
+    else
+    {
+        cout << "Not well" << endl;
+    }
+}
