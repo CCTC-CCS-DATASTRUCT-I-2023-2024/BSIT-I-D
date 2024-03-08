a454df272364867befe63d5b975a81409fa3189e
Anilov Hector P. Climaco <151500404+R3shaded@users.noreply.github.com>
2024-03-08 09:34:56 +0800

    Add files via upload

---
+++ Activities/Activity VI/CLIMACO_ACTIVITY_VI.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    string Transaction;
+    int total;
+
+    cout << "Do you want to start a transaction with me? (yes/no): ";
+    cin >> Transaction;
+
+    while (Transaction == "yes")
+    {
+        int price;
+
+        cout << "Enter the product price: ";
+        cin >> price;
+
+        total += price;
+
+        cout << "Do you want to continue? (yes/no: )";
+        cin >> Transaction;
+    }
+
+    cout << "Total price: " << total;
+}
