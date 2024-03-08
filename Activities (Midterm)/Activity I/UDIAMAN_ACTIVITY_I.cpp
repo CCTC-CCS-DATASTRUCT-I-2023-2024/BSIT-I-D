81ec2971480253499874cb84da14a1951154e023
johnkenneth1 <157591295+johnkenneth1@users.noreply.github.com>
2024-03-08 09:31:20 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/UDIAMAN_ACTIVITY_I.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include string;
+
+int main() {
+    int arraySize = 5;
+    String name[arraySize];
+
+    for (int i = 0; i <= 5; i++){
+        cout <<"Enter name"<<(i + 1)<<":";
+    cin>> names[1];
+    }
+
+
+    cout <<"Enter the number of the list of names you want to display: ";
+    int position;
+    cin << position;
+
+    if (position >= 1 && position <=arraySize){
+        cout << "I WANT YO SELECT" << names [position - 1] << "." endl;
+
+    }else{ 
+        cout << "NOT EROR" << endl;
+    }
+}
