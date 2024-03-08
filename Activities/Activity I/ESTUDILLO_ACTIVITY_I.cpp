60e7c703419a82f60dd87fafa13014ffb25c6a0f
Estudillostephanie <157589357+Estudillostephanie@users.noreply.github.com>
2024-03-08 09:41:26 +0800

    Add files via upload

---
+++ Activities/Activity I/ESTUDILLO_ACTIVITY_I.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+int a=10;
+int b=20;
+int c=30;
+
+int Sum = a + b;
+int Sub = c - c;
+int Mul = a * b;
+int Div = a / c;
+
+cout << "Sum = " << Sum << endl;
+cout << "Mul = " << Mul << endl; 
+cout << "Div = " << Div << endl;
+cout << "Sub = " << Sub << endl;
+} 
