c010ac54cf4c4babf204357fbe949f5f05127134
joshcapz <156910254+joshcapz@users.noreply.github.com>
2024-03-08 09:37:13 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/CAPACITE_ACTIVITY_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string name[5];
+    cout <<"Enter name: ";
+
+    for (int i = 0; i < 5; i++){
+        cout <<"Enter name"<< i = 1 << ":" ;
+       
+        getline (cin >> sw, name[1]) ;
+    }
+    cout <<"Select a number from the list: ";
+    int position;
+
+    if(position >= 1 && position <= 5)
+    {
+    cout <<  names[position-1];
+    }else{
+        cout <<"NO NAME" ;
+    }
+
+    return 0;
+}
+
+
+     
+
+    
