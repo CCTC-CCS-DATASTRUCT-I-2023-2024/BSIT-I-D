fe4dd70440c517b0b8fc6c48ea38973a67562fbc
shekinamariemaurin <156909161+shekinamariemaurin@users.noreply.github.com>
2024-03-08 09:28:09 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/MAURIN_ACTIVITY_I.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    string names[5];
+
+
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter name " << (i + 1) << ": ";
+        cin >> (names[i]);
+    }
+
+    cout << "Enter the number of the name you would like to select (1-5): ";
+
+    int num;
+    cin >> num;
+     
+    if (num >= 1 && num<= 5) {
+        cout << "Selected name: " << names[num - 1] << endl;
+    } else {
+        cout << "Sorry, your selection is invalid . Please try again." << endl;
+    }
+
+}
