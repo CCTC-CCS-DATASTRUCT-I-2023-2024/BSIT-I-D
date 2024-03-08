908c9df8d9500c82c94c6bb337a02dad0298f764
shekinamariemaurin <156909161+shekinamariemaurin@users.noreply.github.com>
2024-03-08 09:38:02 +0800

    Add files via upload

---
+++ Activities/Activity V/MAURIN_ACTIVITY_V.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age>= 0 && age <=3){
+        cout << "Free" <<endl;
+
+    }else if (age  >= 4 && age<=6){
+        cout << "Discounted" <<endl;
+    }else
+        cout << "Normal" <<endl;
+
+
+    
+}
