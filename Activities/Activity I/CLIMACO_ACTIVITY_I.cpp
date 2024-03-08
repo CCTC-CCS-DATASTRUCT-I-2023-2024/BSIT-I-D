62ee5cb6ba0ffd83d872344f9785512416db1b65
Anilov Hector P. Climaco <151500404+R3shaded@users.noreply.github.com>
2024-03-08 09:32:43 +0800

    Add files via upload

---
+++ Activities/Activity I/CLIMACO_ACTIVITY_I.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x = 30;
+    int y = 40;
+    int z = 50;
+
+    int Product = x * y * z;
+    int Sum = x + y + z;
+    int Difference = x - y - z;
+    int Qoutient = x / y / z;
+
+    cout << "Product is: " << Product << endl;
+    cout << "Sum is: " << Sum << endl;
+    cout << "Difference is: " << Difference << endl;
+    cout << "Qoutient is: " << Qoutient << endl;
+}
