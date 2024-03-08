d1385bb082bbc097e27e6e47245f6ecdb6d38611
alissonperales <157588373+alissonperales@users.noreply.github.com>
2024-03-07 17:30:57 -0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/PERALES_ACTIVITY_I.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string name[5];
+    cout <<"Enter name";
+
+    for (int i = 0; i < 5; i++ ){
+
+        cout <<"Enter name"<<(i + 1) << ":" ;
+        cin >> names[i];
+    }
+    cout <<"Select a number from the list: ";
+    int position;
+    cin >> position;
+
+    if(position >= 1 && position <= 5){
+
+    cout << "You have selected" names[position - 1] << endl;
+    
+    
+    cout << names[position-1];
+    }else{
+        cout <<"Invalid Index";
+    }
+}
