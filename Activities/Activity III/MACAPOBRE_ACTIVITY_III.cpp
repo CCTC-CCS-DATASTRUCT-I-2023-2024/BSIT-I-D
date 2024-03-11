965f71ba9f84f06456509adeb62ea49bfc9fd516
Mikeangelo1 <159012283+Mikeangelo1@users.noreply.github.com>
2024-03-11 03:10:03 -0700

    Add files via upload

---
+++ Activities/Activity III/MACAPOBRE_ACTIVITY_III.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int age;
+    cout <<"Enter your age:";
+    cin >> age;
+    if ( age >= 16){
+        cout <<"Allowed" << endl;
+    }else{
+        cout << "Not Allowed" << endl;
+    }
+
+}
