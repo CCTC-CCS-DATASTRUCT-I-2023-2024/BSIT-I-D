fb53e4d0c96c50b86793d90c64f993717ab3492f
Superangelo123 <157589468+Superangelo123@users.noreply.github.com>
2024-03-18 18:27:04 +0800

    Add files via upload

---
+++ Activities/Activity II/POGIO_ACTIVITY_ll.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+
+    cout << "Enter your value: ";
+    cin >> num1;
+
+    cout << "Enter your value: ";
+    cin >> num2;
+
+    int sum = num1 + num2;
+    int Prod = num1 * num2;
+    int Qou = num1 - num2;
+    int Diff = num1 / num2;
+
+    cout << "Sum is: " << sum << endl;
+    cout << "Product is: " << Prod << endl;
+    cout << "Qoutient is: " << Qou << endl;
+    cout << "Difference is: " << Diff << endl;
+}
