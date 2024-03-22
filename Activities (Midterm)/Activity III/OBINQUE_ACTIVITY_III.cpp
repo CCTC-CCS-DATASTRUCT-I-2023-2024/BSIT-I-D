2892aecdd8732cfed7bb714e6ab939f5444a5f56
obinque <157590125+obinque@users.noreply.github.com>
2024-03-22 09:25:03 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/OBINQUE_ACTIVITY_III.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int scores[5][5];
+    int sum = 0;
+    int num = 0;
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            scores[i][j] = 0;
+        }
+    }
+
+    cout << "Enter scores from 1-25:";
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << "Enter score for row " << i + 1 << " and column " << j + 1 << ":  ";
+            cin >> scores[i][j];
+            sum += scores[i][j];
+            num++;
+        }
+    }
+
+    double average = sum / num;
+    cout << "Average score: " << average;
+
+   
+}
