2c5c5dc6d35b548d095abb95779284d84fef6d9f
allegofaith <156910544+allegofaith@users.noreply.github.com>
2024-03-22 10:10:46 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/ACTIVITY_ALLEGO_III_MID.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int scores [5][5];
+
+cout << "Enter the scores: ";
+ for(int i=0; i < 5; i++){
+    for (int j=0; j <5; j++){
+      cin >> scores[i][j];
+    }
+ }
+ int sum =0;
+  for (int i =0; i <5; i++){
+    for (int j =0; j< 5; j++ ){
+        sum += scores[i][j];
+    }
+}
+
+double average = (double) sum/25;
+ cout << "Average : " << average << endl;
+
+
+
+}
