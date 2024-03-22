cec1bca69be3aaa2e20e644de599f6c825e271b2
alissonperales <157588373+alissonperales@users.noreply.github.com>
2024-03-22 10:03:22 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity II/ARINGAY_ACTIVITY_II.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+        cout << ("Enter score: ")<< endl;
+        double score[10]; 
+        double average;
+        double sum=0;
+
+        for(int i=0; i<10;i++){
+          cout << ("Enter your score: ");
+          cin >> score [i];
+          sum += score[i];
+
+        }
+        average=sum/10;
+         cout << "Average: " << average << endl;
+    
+}
