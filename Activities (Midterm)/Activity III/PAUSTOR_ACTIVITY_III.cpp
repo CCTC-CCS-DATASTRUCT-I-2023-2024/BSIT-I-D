d881d3f7a07accf4733afd6d6cdb6901151d7056
carl160 <156910039+carl160@users.noreply.github.com>
2024-03-22 09:16:28 +0800

    PAUSTOR_ACTIVITY_III.cpp

---
+++ Activities (Midterm)/Activity III/PAUSTOR_ACTIVITY_III.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+     int scores [5][5];
+
+        for(int i = 0; i < 5; i++) {
+            cout << "Enter scores for student " << (i + 1) << ": " << endl;
+            for(int j = 0; j < 5; j++) {
+            cout << "Enter score " << (j + 1) << ": " << endl;
+                cin >> scores[i][j];
+            }
+        }
+
+        int sum = 0;
+        for(int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                sum += scores[i][j];
+            }
+        }
+
