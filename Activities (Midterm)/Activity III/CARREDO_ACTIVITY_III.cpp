06002ea24fdd60bc6a9ba50a805a5d308d488de8
LuckyCarredo <159015794+LuckyCarredo@users.noreply.github.com>
2024-03-22 09:50:30 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/CARREDO_ACTIVITY_III.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int scores[5][5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Enter scores for players " << (i + 1) << ": " << endl;
+        for (int j = 0; j < 5; j++)
+        {
+            cout << "Enter score " << (j + 1) << ": ";
+            cin >> scores[i][j];
+        }
+    }
+
+
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            sum += scores[i][j];
+        }
+    }
+
+
+    double average = (double)sum / (5 * 5);
+
+    cout << "Average Score: " << average;
+}
