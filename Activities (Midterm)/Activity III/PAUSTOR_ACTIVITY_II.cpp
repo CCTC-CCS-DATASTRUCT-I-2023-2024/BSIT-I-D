17a066929793587b45dec4e4e85b586988d10629
carl160 <156910039+carl160@users.noreply.github.com>
2024-03-22 09:14:14 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/PAUSTOR_ACTIVITY_II.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double scores [10];
+     for(int s = 0; s < 10; s++){
+            cout << "Enter Score " << (s + 1) << ": ";
+            cin >> scores[s];
+
+        }
+         double sum = 0;
+        for(double s: scores){
+            sum += s;
+        }
+        double average =  sum / scores[s];
+        cout << "Average Score: " << average;
+
+}
