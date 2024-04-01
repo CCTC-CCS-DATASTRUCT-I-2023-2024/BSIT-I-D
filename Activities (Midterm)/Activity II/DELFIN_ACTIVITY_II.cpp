98f9b7b5bbf420c440c21d545011efc9f4bf9617
warr90 <158041134+warr90@users.noreply.github.com>
2024-04-01 16:37:42 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity II/DELFIN_ACTIVITY_II.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+        cout << ("Enter score: ")<< endl;
+        double score[10]; 
+        double average;
+        double sum=0;
+
+        for(int x=0; x<10;x++){
+          cout << ("Enter your score: ");
+          cin >> score [x];
+          sum += score[x];
+
+        }
+        average=sum/10;
+         cout << "Average: " << average << endl;
+    
+}
+
