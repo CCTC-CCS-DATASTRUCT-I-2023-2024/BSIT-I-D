958bd773c235379b609cfc0a22a4bff8b2536773
xelajah17 <157589809+xelajah17@users.noreply.github.com>
2024-04-25 20:59:56 -0700

    Add files via upload

---
+++ Activities (Midterm)/Activity I/QUIAPO_ACTIVITY_I.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string names[5];
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter name " << (i + 1) << ": ";
+        cin >> (names[i]);
+    }
+
+    cout << "Enter the number of the name you would like to select (1-5): ";
+
+    int num;
+    cin >> num;
+     
+    if (num >= 1 && num<= 5) {
+        cout << "Selected name: " << names[num - 1] << endl;
+    } else {
+        cout << "Sorry, your selection is invalid . Nice try!." << endl;
+    }
+
+}
