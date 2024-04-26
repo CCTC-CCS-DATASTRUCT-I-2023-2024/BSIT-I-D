60ebf925c0d09aefa3c7bc524cb5fb0dec341dd9
xelajah17 <157589809+xelajah17@users.noreply.github.com>
2024-04-25 20:53:39 -0700

    Add files via upload

---
+++ Activities/Activity I/QUIAPO_ACTIVITY_I.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a = 10;
+    int b = 15;
+    int c = 20;
+
+    int sum = a+b;
+    int difference = c-b;
+    int product = b*b;
+    int quotient = c/a;
+
+    cout <<"Sum is " << sum << endl;
+    cout <<"Difference is "
