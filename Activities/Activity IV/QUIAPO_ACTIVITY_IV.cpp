54c4db4f9701ac8a6d244cbc9f40a47a214f6335
xelajah17 <157589809+xelajah17@users.noreply.github.com>
2024-04-25 20:54:38 -0700

    Add files via upload

---
+++ Activities/Activity IV/QUIAPO_ACTIVITY_IV.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+    cout<< "Enter your coffe choice: ";
+    cin >> num;
+
+    switch(num){
+        case 1:
+        cout << "Espresso" <<endl;
+        break;
+        case 2:
+        cout << "Americano" <<endl;
+        break;
+        case 3:
+        cout << "Cappuccino" <<endl;
+        break;
+        case 4:
+        cout << "Latte" <<endl;
+        break; 
+    }
+
+}
