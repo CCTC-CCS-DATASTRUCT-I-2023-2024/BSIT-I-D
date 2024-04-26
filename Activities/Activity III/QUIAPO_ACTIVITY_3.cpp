de440de50f5492549e94fd04b186bc91a938dba4
xelajah17 <157589809+xelajah17@users.noreply.github.com>
2024-04-25 20:24:26 -0700

    Add files via upload

---
+++ Activities/Activity III/QUIAPO_ACTIVITY_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+   int temp;
+   cout<<"Enter your age: ";
+   cin>>"age";
+
+   if(temp>=16){
+       cout<<"Welcome";
+     }else {
+      cout<<"Not allowed";
+     }
+    
+}
