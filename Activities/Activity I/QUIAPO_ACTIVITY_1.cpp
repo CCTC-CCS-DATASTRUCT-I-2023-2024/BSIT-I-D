ab0e862c88fb9de6025460b164bfbdeacfe4bc46
xelajah17 <157589809+xelajah17@users.noreply.github.com>
2024-04-25 20:23:48 -0700

    Add files via upload

---
+++ Activities/Activity I/QUIAPO_ACTIVITY_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int a = 10, b= 20, c= 30;
+  
+int sum = a + b + c;
+int diff = a - b - c;
+int product = a*b*c;
+int quotient = a/b/c;
+
+cout<< sum << endl;
+cout<< diff << endl;
+cout<< product << endl;
+cout<< quotient << endl;
+
+
+
+}
