d8c7f4e8a92d5d1668870721419fc5f75f01ddd6
kesielynmacapaz <157589229+kesielynmacapaz@users.noreply.github.com>
2024-05-16 20:21:05 +0800

    Create MACAPAZ_ACTIVITY_ll.cpp

---
+++ Activities/Activity II/MACAPAZ_ACTIVITY_ll.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int fnum, snum, sum, diff, pro,quo;
+    cout << "Enter your first number:";
+    cin >> fnum;
+    cout << "Enter your second number:";
+    cin >> snum;
+    sum = fnum + snum;
+    diff = fnum - snum;
+    pro = fnum * snum;
+    quo = fnum / snum;
+    cout << "Sum: " << sum << endl;
+    cout << "Difference: " << diff << endl;
+    cout << "Product: " << pro << endl;
+    cout << "Quotient: " << quo << endl;
+
+    return 0;
+}
