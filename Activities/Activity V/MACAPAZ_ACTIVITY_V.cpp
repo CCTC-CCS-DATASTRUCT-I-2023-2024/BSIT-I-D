f204298807dc74cad3833215cb4c3d39e7934a9a
kesielynmacapaz <157589229+kesielynmacapaz@users.noreply.github.com>
2024-05-16 20:14:19 +0800

    Create MACAPAZ_ACTIVITY_V.cpp

---
+++ Activities/Activity V/MACAPAZ_ACTIVITY_V.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int age;
+  cout<<"Enter your age: ";
+  cin>>age;
+
+  if( age<=3 && age>=0){
+    cout<<" free";
+    } else if ( age<=6 && age>=4 ){
+    cout<<" discounted";   
+    }else{
+    cout<<" normal ";
+    }
+}
+
